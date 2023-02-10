6c321da67803c14963b27c1d8868bab778afacbf
Konsantinos Tsalapatas <tsalapatas@user57.strw.leidenuniv.nl>
2023-02-10 14:15:31 +0100

    modified 1_2

--- NUR_Tutorial_1/NUR_Tutorial_1_2.py
+++ NUR_Tutorial_1/NUR_Tutorial_1_2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from timeit import timeit
 
+
 c = 3*10**8
 # computes rs using c
 def rs_slow(m):
@@ -27,3 +28,20 @@ if __name__ in "__main__":
     t2 = timeit(s2, globals = globals())
     print("Slower run time: ", t1)
     print("Faster run time: ", t2)
+    
+#new dditions to the code
+
+def factorial(num):
+    '''
+    Documentation: FILL IN
+    '''
+    
+    #ACCOUNT FOR SPECIAL CASES OF INPUT
+    #E.G. NEGATIVES, FLOATS, 0,  ETC
+    
+    factorial=1
+    
+    for i in range(1,num+1):
+       factorial*=i
+    
+    return factorial
